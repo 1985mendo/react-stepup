9e2a84bb81458fc7f5bbaf0073b65c883e4bdead
1985mendo <119387446+1985mendo@users.noreply.github.com>
2023-02-09 11:04:24 +0900

    Initial commit

---
+++ src/components/templates/HeaderOnly.jsx
@@ -0,0 +1,12 @@
+import { Header } from "../atoms/layout/Header";
+
+export const HeaderOnly = (props) => {
+  const { children } = props;
+  return (
+    <>
+      <Header />
+      <div style={{ height: "50px", backgroundColor: "red" }}></div>
+      {children}
+    </>
+  );
+};
